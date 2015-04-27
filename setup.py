3c0e206cb61c5257ab4689fe3316b5e3fd6f6776
Agustín Benassi <agusbenassi@gmail.com>
2015-04-27 12:14:40 -0400

    Fix setup.py.

--- setup.py
+++ setup.py
@@ -11,19 +11,18 @@ except ImportError:
 with open('README.md') as readme_file:
     readme = readme_file.read()
 
-requirements = [
-    # TODO: put package requirements here
-]
+with open("requirements.txt") as f:
+    requirements = [req.strip() for req in f.readlines()]
 
 test_requirements = [
-    # TODO: put package test requirements here
+    "nosetests"
 ]
 
 setup(
     name='xlseries',
     version='0.1.0',
     description="Python package to scrape data series from excel files.",
-    long_description=readme + '\n\n' + history,
+    long_description=readme,
     author="Agustin Benassi",
     author_email='agusbenassi@gmail.com',
     url='https://github.com/abenassi/xlseries',
@@ -46,6 +45,6 @@ setup(
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7'
     ],
-    test_suite='tests',
+    test_suite='nosetests',
     tests_require=test_requirements
 )
