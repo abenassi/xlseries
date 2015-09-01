2ebb51d1888af01d34d8d932a12f7b074ca616a7
Agustín Benassi <agusbenassi@gmail.com>
2015-09-01 20:28:21 -0300

    Improve unicode support for context keys, written by the user.

--- xlseries/strategies/discover/parameters.py
+++ xlseries/strategies/discover/parameters.py
@@ -619,7 +619,7 @@ class Parameters(object):
         for hc_context, header_coord in zip(new_context, headers_coord):
             for context_item in ordered_context:
                 if coord_in_scope(header_coord, context_item[1]):
-                    hc_context.append(unidecode(context_item[0]))
+                    hc_context.append(context_item[0].decode("utf-8"))
 
         return new_context
 
